15d0eb935be47f1e3291ed4608d250b4edef7a66
Cassandra112 <cassandra.elena.soto@gmail.com>
2021-01-25 10:57:05 -0800

    js updates

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,3 +1,9 @@
 console.log("Hello World!");
+console.log('Favorite Websites.'');
+console.log('www.google.com. & www.youtube.com & www.spotify.com');
+console.log('Favorite Games.');
+console.log('Pinball Machines & Mario Bros. & PacMan.');
+console.log('Favorite Artists.');
+console.log('The Fray & Eli Young Band & Whitney Houston.');
 // If you open the web console,
 // you will see "Hello World!" printed, just like before.
